a08072d7fea9ac8540517764165a3f6d8cb51f0f
Corey Moore <MOO19008510@btc.ac.uk>
2024-06-02 02:20:15 +0100

    Products not working

--- shop/products.php
+++ shop/products.php
@@ -2,6 +2,14 @@
 
 <?php require __DIR__ . "/inc/header.php"; ?>
    
+<?php
+ require_once './inc/functions.php'; //Make sure the function php file is there
+ $products =$controllers->products()->get_all_products(); //Retrieve all products from the get_all_products from the ProductController and store in $products
+ foreach ($products as $product) {//For each product in $product
+   echo '<h1>'. $product[0] .'</h1><h2>'. $product[1] . '</h2>';//Output the data for the selected product
+ }
+?>
+
    <h1>Our Products!</h1>
    <p>In this display we show all of our current flowers, this will be updated each season!</p>
 
