007221b993140e8e1b1153ab34bc09c9463bdf3f
Corey Moore <MOO19008510@btc.ac.uk>
2024-06-02 02:21:44 +0100

    Products appearing, FIXED

--- shop/products.php
+++ shop/products.php
@@ -6,7 +6,7 @@
  require_once './inc/functions.php'; //Make sure the function php file is there
  $products =$controllers->products()->get_all_products(); //Retrieve all products from the get_all_products from the ProductController and store in $products
  foreach ($products as $product) {//For each product in $product
-   echo '<h1>'. $product[0] .'</h1><h2>'. $product[1] . '</h2>';//Output the data for the selected product
+   echo '<h1>'. $product['id'] .'</h1><h2>'. $product['name'] . '</h2>';//Output the data for the selected product
  }
 ?>
 
